9575ec225082172dce8a53336d419d458b315133
castillocodes <janeirosergio@gmail.com>
2021-09-05 10:19:29 -0500

    Revert "Deleted hooks, added classes"
    This reverts commit fc2d7e34eecba481ba032a2799929adc1112b243.

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { useParams } from 'react-router-dom';
 // Config
 import { IMAGE_BASE_URL, POSTER_SIZE } from '../config';
@@ -9,80 +9,43 @@ import Spinner from './Spinner';
 import MovieInfo from './MovieInfo';
 import MovieInfoBar from './MovieInfoBar';
 import Actor from './Actor';
+// Hook
+import { useMovieFetch } from '../hooks/useMovieFetch';
 // Image
 import NoImage from '../images/no_image.jpg';
-// API
-import API from '../API';
 
-class Movie extends Component {
-    state = {
-        movie: {},
-        loading: true,
-        error: false
-    };
-    fetchMovie = async () => {
-        const { movieId } = this.props.params;
+const Movie = () => {
+    const { movieId } = useParams();
 
-        try {
-            this.setState({ error: false, loading: true });
-
-            const movie = await API.fetchMovie(movieId);
-            const credits = await API.fetchCredits(movieId);
-            // Get directors only
-            const directors = credits.crew.filter(
-                member => member.job === 'Director'
-            );
-
-            this.setState({
-                movie: {
-                    ...movie,
-                    actors: credits.cast,
-                    directors
-                },
-                loading: false,
-            });
-        } catch (error) {
-            this.setState({error: true, loading: false });
-        }
-    };
-
-    componentDidMount() {
-        this.fetchMovie();
-    }
-
-    render() {
-        const { movie, loading, error } = this.state;
-
-        if (loading) return <Spinner />;
-        if (error) return <div>Something went wrong...</div>;
+    const { state: movie, loading, error } = useMovieFetch(movieId)
     
-        return (
-            <>
-                <BreadCrumb movieTitle={movie.original_title} />
-                <MovieInfo movie={movie} />
-                <MovieInfoBar 
-                    time={movie.runtime} 
-                    budget={movie.budget} 
-                    revenue={movie.revenue} 
-                />
-                <Grid header='Actors'>
-                    {movie.actors.map(actor => (
-                        <Actor
-                            key={actor.credit_id}
-                            name={actor.name}
-                            character={actor.character}
-                            imageUrl={
-                                actor.profile_path
-                                ? `${IMAGE_BASE_URL}${POSTER_SIZE}${actor.profile_path}` : NoImage
-                            }
-                        />
-                    ))}
-                </Grid>
-            </>
-        );
-    }
-}
+    if (loading) return <Spinner />;
+    if (error) return <div>Something went wrong...</div>;
 
-const MovieWithParams = props => <Movie { ...props} params={useParams()} />
+    return (
+        <>
+            <BreadCrumb movieTitle={movie.original_title} />
+            <MovieInfo movie={movie} />
+            <MovieInfoBar 
+                time={movie.runtime} 
+                budget={movie.budget} 
+                revenue={movie.revenue} 
+            />
+            <Grid header='Actors'>
+                {movie.actors.map(actor => (
+                    <Actor
+                        key={actor.credit_id}
+                        name={actor.name}
+                        character={actor.character}
+                        imageUrl={
+                            actor.profile_path
+                            ? `${IMAGE_BASE_URL}${POSTER_SIZE}${actor.profile_path}` : NoImage
+                        }
+                    />
+                ))}
+            </Grid>
+        </>
+    );
+};
 
-export default MovieWithParams;
+export default Movie;
